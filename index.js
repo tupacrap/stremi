9ec4de1e584bdce0720a61279892fd4e2ed70f5c
tupacrap <69865788+tupacrap@users.noreply.github.com>
2024-11-18 16:39:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const { serveHTTP, getRouter } = require('stremio-addon-sdk');
+const { getRouter } = require('stremio-addon-sdk');
 const addonInterface = require('./addon');
+const router = getRouter(addonInterface);
 
-module.exports = getRouter(addonInterface);
+module.exports = (req, res) => {
+    router(req, res, () => {
+        res.statusCode = 404;
+        res.end();
+    });
+};
